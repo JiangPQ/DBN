54decfbe653c1c3b9019d64ee61b0878c53328b0
JiangPQ <jiangpq4v@gmail.com>
2017-10-07 13:18:19 +0800

    add BaseOperation

---
+++ src/operation/BaseOperation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jiangpq on 17-10-6.
+//
+
+#include "BaseOperation.h"
---
+++ src/operation/BaseOperation.h
@@ -0,0 +1,16 @@
+//
+// Created by jiangpq on 17-10-6.
+//
+
+#ifndef DBN_BASEOPERATION_H
+#define DBN_BASEOPERATION_H
+
+
+class BaseOperation {
+private:
+public:
+    BaseOperation();
+};
+
+
+#endif //DBN_BASEOPERATION_H
