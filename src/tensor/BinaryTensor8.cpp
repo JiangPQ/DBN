abbdc12be135aeabd66677e8fe1a0ca76662e883
JiangPQ <jiangpq4v@gmail.com>
2017-10-09 18:16:16 +0800

    fix bugs

--- src/tensor/BinaryTensor8.cpp
+++ src/tensor/BinaryTensor8.cpp
@@ -4,18 +4,37 @@
 
 #include "BinaryTensor8.h"
 
-BinaryTensor8::BinaryTensor8(long n_dimension, const long *dimensions, bool is_weight = false) : BinaryTensor(
-        n_dimension, dimensions, is_weight) {
+BinaryTensor8::BinaryTensor8(long n_dimension, const long *dimensions, bool is_weight = false) : Tensor(
+        n_dimension, dimensions) {
 
-    long int length = 1;
-    for(int i = 0; i < n_dimension - 1; i++){
-        length *= dimensions[i];
+    this->is_weight = is_weight;
+
+    // compute how many last dimensions are there
+    this->last_dim_num = 1;
+    for (long i = 0; i < n_dimension - 1; ++i) {
+        this->last_dim_num *= dimensions[i];
     }
-    length *= this->get_actual_last_dim_len();
-    this->storage = new unsigned char(length);
+
+    // compute the length the last dimension
+    long rem = dimensions[n_dimension - 1] % 8;
+    // if this tensor is a 3 by 3 weight, then cut it to length 8
+    this->last_dim_len = dimensions[n_dimension - 1] / 8 + (!this->is_weight && rem >= 4) ? 1 : 0;
+
+    this->storage = new unsigned char(this->last_dim_num * this->last_dim_len);
 }
 
 BinaryTensor8::~BinaryTensor8() {
     delete this->storage;
 }
 
+long BinaryTensor::get_actual_last_dim_len() {
+    return this->last_dim_len;
+}
+
+long BinaryTensor::get_how_many_last_dim() {
+    return this->last_dim_num;
+}
+
+bool BinaryTensor::get_is_weight() {
+    return this->is_weight;
+}
--- src/tensor/BinaryTensor8.h
+++ src/tensor/BinaryTensor8.h
@@ -9,16 +9,25 @@
 #include "Tensor.h"
 #include "BinaryTensor.h"
 
-class BinaryTensor8: public BinaryTensor {
+class BinaryTensor8 : public Tensor {
     friend class BinarizationOp8;
     friend class FloatenOp;
 
     friend class BinConv2d;
 private:
+    long last_dim_len;
+    long last_dim_num;
+    bool is_weight;
     unsigned char * storage;
 public:
     BinaryTensor8(long n_dimension, const long *dimensions, bool is_weight = false);
     ~BinaryTensor8() final ;
+
+    long get_actual_last_dim_len();
+
+    long get_how_many_last_dim();
+
+    bool get_is_weight();
 };
 
 
